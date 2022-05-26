aae080fe9ea931e00773f419645a334c9e05d499
maxkl <maxklim929@gmail.com>
2022-05-26 17:23:04 -0400

    V1

--- Window.java
+++ Window.java
@@ -1,4 +1,7 @@
-public static void main(String[] args)
+public class Window
 {
- System.out.println("Hello World");
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	}
 }
